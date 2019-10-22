ffe941bbfd1c43f31a4418fb7712317084a2ebc9
JJE <elguetaj@ufm.edu>
2019-10-21 18:15:18 -0600

    adds requests

--- myapp/lmm.py
+++ myapp/lmm.py
@@ -1,23 +1,33 @@
 from flask import Flask, render_template, url_for
 import os
 import redis
+from rq import Queue
+import requests
+import json
+
 
 app = Flask(__name__)
 
-client = redis.Redis('localhost','6379',0)
-client.set('language', 'python')
+#client= redis.Redis('localhost',6379,0)
+#q = Queue(connection=client)
 
 
 @app.route('/')
 @app.route('/home')
 
 def home():
-    return render_template('home.html', client= client)
+    r = requests.get('https://api.themoviedb.org/3/trending/movie/week?api_key=1b4b5da3860135ee31a089ea237baae3')
+    print(r.text)
+    return render_template('home.html', movies=json.loads(r) )
+
+
 
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
+
 
 #deployed to: https://young-refuge-73995.herokuapp.com
--- myapp/redis_client.py
+++
@@ -1,9 +0,0 @@
-import redis
-
-client = redis.Redis('localhost','6379',0)
-
-
-#demo 
-
-client.set('language', 'python')
-print(client.get('language'))
