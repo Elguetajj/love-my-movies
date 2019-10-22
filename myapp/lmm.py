f1a3bc39c90f89c688f8ddaa61a378a0a118ef3f
JJE <elguetaj@ufm.edu>
2019-10-21 23:31:29 -0600

    adds Tv and Sci-fi routes

--- myapp/lmm.py
+++ myapp/lmm.py
@@ -9,7 +9,6 @@ import json
 app = Flask(__name__)
 
 #client= redis.Redis('localhost',6379,0)
-#q = Queue(connection=client)
 
 
 @app.route('/')
@@ -18,6 +17,18 @@ app = Flask(__name__)
 def home():
     r = requests.get('https://api.themoviedb.org/3/trending/movie/week?api_key=1b4b5da3860135ee31a089ea237baae3')
     return render_template('home.html', movies=json.loads(r.text) )
+
+
+@app.route('/tv')
+def tv():
+    r = requests.get('https://api.themoviedb.org/3/trending/tv/week?api_key=1b4b5da3860135ee31a089ea237baae3')
+    return render_template('home.html', movies=json.loads(r.text))
+
+@app.route('/Sci-fi')
+def Sci_fi():
+    r = requests.get('https://api.themoviedb.org/3/discover/movie?api_key=1b4b5da3860135ee31a089ea237baae3&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=%20878')
+    return render_template('home.html', movies=json.loads(r.text))
+
 
 
 
